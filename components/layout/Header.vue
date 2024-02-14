fae7030d15fc35d8404bc5e928567e0433899ca8
mayoforkovic <mayo@cvernovka.com>
2024-02-14 10:18:38 +0100

    feat: add new components and layout for image handling and display
    - Introduced a new `Image.vue` component to handle image loading with lazy loading support
    - Created `Item.vue` component to display item details including author, title, and dating
    - Added a `Header.vue` layout component featuring logo, title, search bar, and optional frontpage image
    - Implemented the default layout in `default.vue`, including header and slot for content
    - Set up the main page (`index.vue`) with filter, sort options, masonry wall for items, and pagination controls
    - Integrated MasonryWall plugin in `general.ts`
    - Added placeholder image file for use in various components

---
+++ components/layout/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="bg-dark flex flex-col items-center w-full">
+    <div class="px-4 pt-3 pb-6 md:px-16 md:pt-6 md:pb-12 w-full max-w-[1320px]">
+      <NuxtLink to="/" class="block md:flex gap-8 items-end">
+        <div><img :src="Logo" alt="Moravská galerie" /></div>
+        <div><img class="max-w-[66%] md:max-w-full" :src="Title" alt="Sbírky on-line" /></div>
+      </NuxtLink>
+
+      <SearchBar class="mt-8" />
+
+      <TransitionExpand>
+        <Image v-if="isFrontpage" url="/placeholder.png" class="aspect-[9/4] mt-8 object-cover" />
+      </TransitionExpand>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import Logo from '~/assets/svg/logo.svg'
+import Title from '~/assets/svg/title.svg'
+import SearchBar from '~/components/general/SearchBar.vue'
+import Image from '~/components/general/Image.vue'
+
+const route = useRoute()
+
+const isFrontpage = computed(() => route.path === '/')
+</script>
