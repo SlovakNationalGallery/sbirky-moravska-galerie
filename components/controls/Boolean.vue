3c314c5e1f0885967b52488b1180745cba18e688
mayoforkovic <mayo@cvernovka.com>
2024-03-14 08:51:02 +0100

    feat: update boolean control and add tailwindcss forms
    - Replaced custom checkbox implementation in Boolean.vue with a standard HTML input of type checkbox
    - Removed unused Icon import from Boolean.vue
    - Added @tailwindcss/forms package to the project dependencies
    - Updated tailwind.config.js to include @tailwindcss/forms plugin

--- components/controls/Boolean.vue
+++ components/controls/Boolean.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="flex gap-3 cursor-pointer" @click="onToggle">
-    <div
-      class="text-primary w-6 h-6 border border-black flex items-center transition-all"
-      :class="{ 'bg-primary-light border-primary': model }"
-    >
-      <Icon v-if="model" class="text-primary w-8 h-8" name="check" />
-    </div>
+    <input v-model="model" type="checkbox" />
+
     <div>{{ label }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
-import Icon from '~/components/general/Icon.vue'
 import { useControls } from '~/composables/controls'
 
 const props = withDefaults(
