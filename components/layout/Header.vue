02cdf1ef5704707ccc6af4aa48e988ee87415892
mayoforkovic <mayo@cvernovka.com>
2024-03-14 08:55:58 +0100

    feat: add new components and pages (About, Collections)
    - Added Collection.vue component for displaying individual collections
    - Updated Header.vue with additional navigation links
    - Created about.vue page with gallery information and usage rules
    - Created collection detail page ([id].vue) with artwork details and sorting functionality
    - Created collections index page (index.vue) listing all collections with sorting functionality
    - Added placeholder image to public directory for use in new pages

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,12 +1,24 @@
 <template>
   <header class="bg-dark flex flex-col items-center w-full">
     <div class="px-4 pt-3 pb-6 md:px-16 md:pt-6 md:pb-12 w-full max-w-[1320px]">
-      <NuxtLink to="/" class="block md:flex gap-8 items-end">
-        <div><img :src="Logo" alt="Moravská galerie" /></div>
-        <div><img class="max-w-[66%] md:max-w-full" :src="Title" alt="Sbírky on-line" /></div>
-      </NuxtLink>
+      <div class="flex flex-col md:flex-row items-start md:items-end gap-2 md:gap-5">
+        <NuxtLink to="/" class="block md:flex gap-8 items-end">
+          <div><img :src="Logo" alt="Moravská galerie" /></div>
+          <div><img class="max-w-[66%] md:max-w-full" :src="Title" alt="Sbírky on-line" /></div>
+        </NuxtLink>
+        <div class="grow"></div>
+        <div class="flex gap-3">
+          <NuxtLink to="/" class="text-white uppercase text-xl">Díla</NuxtLink>
+          <NuxtLink to="/collections" class="text-white uppercase text-xl border-b border-white"
+            >Kolekce</NuxtLink
+          >
+          <NuxtLink to="/about" class="text-white uppercase text-xl border-b border-white"
+            >Info</NuxtLink
+          >
+        </div>
+      </div>
 
-      <SearchBar class="mt-8" />
+      <SearchBar class="mt-4 md:mt-8" />
 
       <TransitionExpand>
         <Image v-if="isFrontpage" url="/placeholder.png" class="aspect-[9/4] mt-8 object-cover" />
