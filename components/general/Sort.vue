b803477f872f7a07e4bd3d89511bc4f96ca33a78
mayoforkovic <mayo@cvernovka.com>
2024-03-20 21:40:14 +0100

    feat: enhance sorting functionality
    - Updated `sortBy` and `sortDirection` to accept null values, allowing for more flexible sorting options
    - Added new sort option 'relevance' with null value and direction
    - Renamed 'datování' to 'datování - nejstarší' and added a new option 'datování - najnovejši'
    - Changed the default model reference in Sort.vue to find the current sortBy value or fallback to null
    - Adjusted controlsService in controls.ts to handle null values for sortBy and sortDirection by default
    - Modified filtersQuery computation logic to only include sort parameters if both sortBy and sortDirection are not null

--- components/general/Sort.vue
+++ components/general/Sort.vue
@@ -8,19 +8,21 @@
 <script lang="ts" setup>
 import Dropdown from '~/components/general/Dropdown.vue'
 
-const sortBy = defineModel<string>('sortBy')
-const sortDirection = defineModel<string>('sortDirection')
+const sortBy = defineModel<string | null>('sortBy')
+const sortDirection = defineModel<string | null>('sortDirection')
 
 const sortOptions = [
-  { label: 'poslední změny', value: 'updated_at', direction: 'desc' },
+  { label: 'relevance', value: null, direction: null },
   { label: 'data přidání', value: 'created_at', direction: 'asc' },
   { label: 'názvu', value: 'title', direction: 'asc' },
-  { label: 'datování', value: 'date_earliest', direction: 'asc' },
+  { label: 'datování - nejstarší', value: 'date_earliest', direction: 'asc' },
+  { label: 'datování - najnovejši', value: 'date_earliest', direction: 'desc' },
   { label: 'počtu zobrazení', value: 'view_count', direction: 'desc' },
   { label: 'náhodně', value: 'random', direction: 'asc' },
+  { label: 'poslední změny', value: 'updated_at', direction: 'desc' },
 ] as const
 
-const model = ref(sortOptions[0].value)
+const model = ref(sortOptions.find((option) => option.value === sortBy.value) || null)
 
 const onUpdate = (value: string) => {
   const option = sortOptions.find((option) => option.value === value)
