cc2100396d7dc7c9134042ce41e22d8127ee3d22
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2024-04-24 19:38:30 +0200

    Add collection index

--- pages/collections/index.vue
+++ pages/collections/index.vue
@@ -1,44 +1,45 @@
 <template>
   <div class="flex flex-col items-center">
     <div class="max-w-[740px]">
-      <h2 class="text-3xl md:text-5xl md:text-center mt-10 mb-6">Kolekcie</h2>
+      <h2 class="text-3xl md:text-5xl md:text-center mt-10 mb-6">Kolekce</h2>
     </div>
 
     <div class="w-full my-4 flex">
       <div class="flex-grow">
-        <span class="font-bold">{{ total }}</span> kolekcií
+        <span class="font-bold">{{ total }}</span> kolekcí
       </div>
-
-      <Sort v-model:sort-by="sortBy" v-model:sort-direction="sortDirection" />
     </div>
 
-    <div v-if="items.length" class="w-full">
+    <div v-if="collections.length" class="w-full">
       <masonry-wall
-        :items="items"
+        :items="collections"
         :ssr-columns="3"
         :column-width="300"
         :gap="24"
-        :key-mapper="(item: Item) => item.id"
+        :key-mapper="(collection: Collection) => collection.id"
       >
         <template #default="{ item }">
-          <Collection :key="`item-${item.id}`" :item="item" />
+          <Collection :key="`collection-${item.id}`" :collection="item" />
         </template>
       </masonry-wall>
     </div>
+
+    <Pager v-model="page" :is-loading="isLoading" :last-page="lastPage" />
   </div>
 </template>
 
 <script setup lang="ts">
-import Item from '~/components/general/Item.vue'
-import { useControls } from '~/composables/controls'
-import Sort from '~/components/general/Sort.vue'
+import { useCollections } from '~/composables/collections'
 import Collection from '~/components/general/Collection.vue'
+import Pager from '~/components/general/Pager.vue'
 
-const isReady = ref(false)
-const { items, total, page, lastPage, isLoading, sortBy, sortDirection } = await useControls()
+const { collections, total, page, lastPage, isLoading, refresh, reset } = await useCollections()
 
 onMounted(async () => {
-  await new Promise((resolve) => setTimeout(resolve, 333))
-  isReady.value = true
+  refresh()
+})
+
+onUnmounted(() => {
+  reset()
 })
 </script>
