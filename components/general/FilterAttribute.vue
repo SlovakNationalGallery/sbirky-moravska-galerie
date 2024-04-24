a8fd172143bcb9a66b7f397d57783577bd7f0b53
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2024-04-24 19:38:36 +0200

    Add collection detail

---
+++ components/general/FilterAttribute.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-primary text-white flex items-center py-1 px-3 gap-2 rounded-3xl text-xs">
+    <span class="font-bold">{{ attribute }}:</span>
+    <template v-if="Array.isArray(value)">
+      {{ value.join(', ') }}
+    </template>
+    <template v-else>
+      {{ value }}
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  attribute: string
+  value: string | string[]
+}>()
+</script>
--- pages/collections/[id].vue
+++ pages/collections/[id].vue
@@ -1,36 +1,20 @@
 <template>
-  <div class="flex flex-col items-center">
+  <div v-if="collection" class="flex flex-col items-center">
     <TransitionExpand>
-      <Image v-if="isReady" url="/placeholder.png" class="max-h-[400px] w-full mt-8 object-cover" />
+      <Image :url="collection.header_image_src" class="max-h-[400px] w-full mt-8 object-cover" />
     </TransitionExpand>
 
     <div class="max-w-[740px]">
-      <h2 class="text-3xl md:text-5xl md:text-center mt-10 mb-6">Kolekcie</h2>
+      <h2 class="text-3xl md:text-5xl md:text-center mt-10 mb-6">{{ collection.name }}</h2>
 
-      <div class="md:text-xl md:text-center">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-        galley of type and scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
-        passages, and more recently with desktop publishing software like Aldus PageMaker including
-        versions of Lorem Ipsum.
-      </div>
+      <div class="md:text-xl md:text-center" v-html="collection.text"></div>
 
       <div class="flex justify-center gap-3 my-6">
-        <div
-          class="bg-primary text-white flex items-center py-1 px-3 gap-2 cursor-pointer rounded-3xl"
-        >
-          <div class="text-xs">
-            <span class="font-bold">Autor:</span> Meno Priezvisko, Meno Priezvisko Ml.
-          </div>
-        </div>
-
-        <div
-          class="bg-primary text-white flex items-center py-1 px-3 gap-2 cursor-pointer rounded-3xl"
-        >
-          <div class="text-xs"><span class="font-bold">Technika:</span> Sietotlač, Rinolit</div>
-        </div>
+        <FilterAttribute
+          v-for="(value, attribute) in collection.item_filter"
+          v-bind="{ attribute, value }"
+          :key="attribute"
+        />
       </div>
     </div>
 
@@ -38,38 +22,90 @@
       <div class="flex-grow">
         <span class="font-bold">{{ total }}</span> děl
       </div>
-
       <Sort v-model:sort-by="sortBy" v-model:sort-direction="sortDirection" />
     </div>
-
-    <div v-if="items.length" class="w-full">
+    <div v-if="collection.items.length" class="w-full">
       <masonry-wall
-        :items="items"
+        :items="collection.items"
         :ssr-columns="3"
         :column-width="300"
         :gap="24"
         :key-mapper="(item: Item) => item.id"
       >
         <template #default="{ item }">
-          <Item :key="`item-${item.id}`" :item="item" />
+          <ItemCard :key="`item-${item.id}`" :item="item" />
         </template>
       </masonry-wall>
     </div>
+
+    <Pager v-model="page" :is-loading="isLoading" :last-page="lastPage" />
   </div>
 </template>
 
 <script setup lang="ts">
 import Image from '~/components/general/Image.vue'
-import Item from '~/components/general/Item.vue'
-import { useControls } from '~/composables/controls'
+import Item from '~/models/Item'
+import ItemCard from '~/components/general/Item.vue'
+import Collection from '~/models/Collection'
+import Pager from '~/components/general/Pager.vue'
+import FilterAttribute from '~/components/general/FilterAttribute.vue'
 import Sort from '~/components/general/Sort.vue'
-import Icon from '~/components/general/Icon.vue'
+const nuxtConfig = useRuntimeConfig()
+const route = useRoute()
+const id = route.params.id as string
 
-const isReady = ref(false)
-const { items, total, page, lastPage, isLoading, sortBy, sortDirection } = await useControls()
-
-onMounted(async () => {
-  await new Promise((resolve) => setTimeout(resolve, 333))
-  isReady.value = true
+const collectionData = useFetch<any>(`api/collections/${id}`, {
+  baseURL: nuxtConfig.public.APP_URL,
 })
+
+const collection = computed(() => new Collection(collectionData.data.value?.data))
+
+// todo from url?
+const sortBy = ref(null)
+const sortDirection = ref(null)
+const page = ref(1)
+
+const itemsQuery = computed(() => {
+  const query: Record<string, number | string> = {
+    size: 12,
+    page: page.value,
+  }
+
+  if (sortBy.value && sortDirection.value) {
+    query[`sort[${sortBy.value}]`] = sortDirection.value
+  }
+
+  return query
+})
+
+interface ItemsResponse {
+  data: Item[]
+  total: number
+  last_page: number
+}
+
+const itemsDataFetch = useFetch<ItemsResponse>(`api/collections/${id}/items`, {
+  baseURL: nuxtConfig.public.APP_URL,
+  query: itemsQuery,
+  watch: [itemsQuery],
+  transform: (response) => ({
+    ...response,
+    data: response.data.map((data) => new Item(data)),
+  }),
+  immediate: false,
+})
+
+watch(itemsDataFetch.data, () => {
+  if (page.value == 1) {
+    collection.value.items = itemsDataFetch.data.value?.data ?? []
+  } else {
+    collection.value.items = [...collection.value.items, ...(itemsDataFetch.data.value?.data ?? [])]
+  }
+})
+
+itemsDataFetch.execute()
+
+const total = computed(() => itemsDataFetch.data.value?.total ?? 0)
+const isLoading = computed(() => itemsDataFetch.pending.value)
+const lastPage = computed(() => itemsDataFetch.data.value?.last_page ?? 0)
 </script>
