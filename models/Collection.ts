9558d1b7a8a10f8ed555f0e97f69fbe8c2a9c635
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2024-05-15 02:10:46 +0200

    Parse collection data in model

--- models/Collection.ts
+++ models/Collection.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod'
+
 import type Item from '~/models/Item'
 
 import BaseModel from '@/models/_BaseModel'
@@ -11,6 +13,27 @@ export default class Collection extends BaseModel {
   public declare item_filter: string | string[]
 
   items: Item[] = []
+
+  static parser = z.object({
+    id: z.string(),
+    name: z.string(),
+    text: z.string(),
+    header_image_src: z.string(),
+    header_image_srcset: z.string(),
+    item_filter: z.union([z.string(), z.array(z.string())]),
+  })
+
+  constructor(data: z.infer<typeof Collection.parser>) {
+    const parse = Collection.parser.safeParse(data)
+
+    if (!parse.success) {
+      console.error(parse.error)
+    } else {
+      data = parse.data
+    }
+
+    super(data)
+  }
 
   public get link() {
     return `/collections/${this.id}`
