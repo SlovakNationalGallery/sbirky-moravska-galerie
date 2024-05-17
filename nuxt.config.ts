a1c00323bec49bca7ab3be4a4c556fb32fb6120c
Mayo Horkovic <mayo.horkovic@gmail.com>
2024-05-17 15:10:16 +0200

    feat: integrate sentry for error tracking
    This update integrates Sentry into the application for better error tracking and debugging. The following changes were made:
    - Added new environment variables for Sentry configuration in .env.example
    - Updated nuxt.config.ts to include these new environment variables
    - Installed @sentry/vue package and updated package-lock.json accordingly
    - Created a new plugin file sentry.client.ts to initialize Sentry with the appropriate configurations based on runtime config values

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,6 +46,15 @@ export default defineNuxtConfig({
       API_URL: process.env.API_URL || '/api',
       APP_URL: process.env.APP_URL || '',
       CDN_URL: process.env.CDN_URL || '',
+
+      APP_NAME: process.env.npm_package_name,
+      APP_VERSION: process.env.npm_package_version,
+      APP_REVISION: process.env.CI_COMMIT_SHORT_SHA || '',
+
+      SENTRY_DSN: process.env.SENTRY_DSN || '',
+      SENTRY_SAMPLE_RATE: process.env.SENTRY_SAMPLE_RATE || '1',
+      SENTRY_TRACES_SAMPLE_RATE: process.env.SENTRY_TRACES_SAMPLE_RATE || '0',
+      SENTRY_REPLAY_SAMPLE_RATE: process.env.SENTRY_REPLAY_SAMPLE_RATE || '0.1',
     },
   },
 })
