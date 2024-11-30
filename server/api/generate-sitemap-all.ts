7b8227ae25054ad408b0d249991297afb806d971
Mayo Horkovic <mayo.horkovic@gmail.com>
2024-11-30 16:09:14 +0700

    feat: add sitemap generation functionality
    - Introduced a new event handler for generating sitemaps
    - The handler checks for a valid token before proceeding with the operation
    - It fetches all pages and generates individual sitemaps for each one
    - Returns a status of 'ok' upon successful completion

---
+++ server/api/generate-sitemap-all.ts
@@ -0,0 +1,21 @@
+export default defineEventHandler(async (event) => {
+  const { token } = getQuery(event)
+
+  if (token !== process.env.SITEMAP_TOKEN) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'Page Not Found',
+    })
+  }
+
+  const page = 1
+  const { lastPage } = await $fetch(`/api/generate-sitemap?page=${page}&token=${token}`)
+
+  for (let i = 2; i <= lastPage; i++) {
+    await $fetch(`/api/generate-sitemap?page=${i}&token=${token}`)
+  }
+
+  return {
+    status: 'ok',
+  }
+})
